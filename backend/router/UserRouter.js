65e8992494d338b59da27f95c8589b226227da90
Yury Mikhaylov <drmixe@gmail.com>
2022-06-20 19:06:09 +0500

    fully working authorization

--- backend/router/UserRouter.js
+++ backend/router/UserRouter.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../controller/UserController");
 const passport = require("../config/passport");
+const multer = require('multer');
 
+router.use(multer().none());
 router.post("/register", UserController.register);
 router.post(
   "/login",
